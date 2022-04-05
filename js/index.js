2651fe9362f6fe8c02f1fa3fb908b76d6ae66f06
Antonio Sala Barbosa <sala.antonio2020@gmail.com>
2022-04-05 14:58:53 -0300

    slider-1

---
+++ js/index.js
@@ -0,0 +1,32 @@
+let swiper = new Swiper('.swiper-container', {
+    navigation: {
+        nextEl: '.slider-next',
+        prevEl: '.slider-prev'
+    },
+    slidesPerView: 1,
+    spaceBetween: 10,
+
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+
+    breakpoints: {
+        520: {
+            slidesPerView: 1,
+            spaceBetween: 10,
+        },
+        580: {
+            slidesPerView: 2,
+            spaceBetween: 20,
+        },
+        820: {
+            slidesPerView: 3,
+            spaceBetween: 20,
+        },
+        1140: {
+            slidesPerView: 4,
+            spaceBetween: 40,
+        },
+    }
+});
